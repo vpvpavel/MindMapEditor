5a155b4bbf513e9cbc14e8b57fdf5beeb65c7b99
Pavlo Vorobiov <>
2020-12-09 03:38:10 +0200

    init

---
+++ MindMapEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindMapEditor
+//
+//  Created by Pavlo Vorobiov on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
